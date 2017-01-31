c822017dfaebf6739ae69896a4593facc8bdadc3
Cliff Koh <cliffkoh@users.noreply.github.com>
2017-01-31 14:51:11 -0800

    Remove *.scss.ts which should not be part of the repo

--- src/client/css/mp-components.scss.ts
+++
@@ -1,5 +0,0 @@
-/* tslint:disable */
-import { loadStyles } from '@microsoft/load-themed-styles';
-
-loadStyles([{"rawString":".mp-choicelist .mp-choicelist-item{margin:8px;opacity:.5;-webkit-transition:all .4s ease;transition:all .4s ease;cursor:pointer}.mp-choicelist .mp-choicelist-item:hover{opacity:.8}.mp-choicelist .mp-choicelist-item.selected{opacity:1}"}]);
-/* tslint:enable */
