36e0c6ee435af772455d86f8d1be2b63d755d097
Zichun Koh <zichun@gmail.com>
2020-12-29 17:47:39 -0800

    deployment script fix attempt #3.1415

--- deploy.cmd
+++ deploy.cmd
@@ -108,7 +108,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 :: 4. Build project
 pushd "%DEPLOYMENT_TARGET%"
 echo Building Project in %DEPLOYMENT_TARGET%...
-call :ExecuteCmd !NPM_CMD! run build
+call :ExecuteCmd !NPM_CMD! run deploy
 popd
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
